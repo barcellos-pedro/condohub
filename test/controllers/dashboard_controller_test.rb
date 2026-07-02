3eb5d940af0dfb13d5a4d64a082e84e10b9de3af
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-07-02 09:45:49 -0300

    feat: conditionally display sandbox mode UI in development and add tests for dashboard controller

---
+++ test/controllers/dashboard_controller_test.rb
@@ -0,0 +1,17 @@
+require "test_helper"
+
+class DashboardControllerTest < ActionDispatch::IntegrationTest
+  setup { sign_in_as(users(:one)) }
+
+  test "index renders authenticated content" do
+    get root_path
+    assert_response :success
+  end
+
+  test "index hides sandbox persona UI outside development" do
+    get root_path
+    assert_response :success
+    assert_no_match(/Sandbox Mode/i, response.body)
+    assert_no_match(/Switch Persona/i, response.body)
+  end
+end
