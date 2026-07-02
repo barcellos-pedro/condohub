123f2963aa5e8c859139308d93a874774e57c8e9
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-07-02 09:00:36 -0300

    feat: enhance error pages with custom styling and update tests for error messages

--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -4,24 +4,28 @@ class ErrorsControllerTest < ActionDispatch::IntegrationTest
   test "should render 404 not found page" do
     get "/errors/404"
     assert_response :not_found
-    assert_match(/404 Not Found/, response.body)
+    assert_match(/404/, response.body)
+    assert_match(/couldn't find the page/, response.body)
   end
 
   test "should render 500 internal server error page" do
     get "/errors/500"
     assert_response :internal_server_error
-    assert_match(/500 Internal Server Error/, response.body)
+    assert_match(/500/, response.body)
+    assert_match(/Something went wrong/, response.body)
   end
 
   test "should render 422 unprocessable entity page" do
     get "/errors/422"
     assert_response :unprocessable_entity
-    assert_match(/422 Unprocessable Entity/, response.body)
+    assert_match(/422/, response.body)
+    assert_match(/couldn't be processed/, response.body)
   end
 
   test "accessing a nonexistent route should render the custom 404 page" do
     get "/nonexistent-path-that-does-not-exist"
     assert_response :not_found
-    assert_match(/404 Not Found/, response.body)
+    assert_match(/404/, response.body)
+    assert_match(/couldn't find the page/, response.body)
   end
 end
