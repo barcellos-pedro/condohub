cb4a056cd19b54083820a9fe33a4634a84771b76
Pedro Barcellos dos Reis <pedrobarcellosdosreis@gmail.com>
2026-07-27 21:03:09 -0300

    feat: add edit/update/destroy actions with authorization to service listings controller

--- app/controllers/service_listings_controller.rb
+++ app/controllers/service_listings_controller.rb
@@ -1,4 +1,7 @@
 class ServiceListingsController < ApplicationController
+  before_action :set_service_listing, only: [ :edit, :update, :destroy ]
+  before_action :require_owner, only: [ :edit, :update, :destroy ]
+
   def create
     @service = current_condominium.service_listings.new(service_params)
     @service.user = current_user
@@ -10,7 +13,33 @@ class ServiceListingsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @service_listing.update(service_params)
+      redirect_to dashboard_path(tab: "services"), notice: t("flash.service_listings.update_success")
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @service_listing.destroy
+    redirect_to dashboard_path(tab: "services"), notice: t("flash.service_listings.destroy_success")
+  end
+
   private
+
+  def set_service_listing
+    @service_listing = current_condominium.service_listings.find(params[:id])
+  end
+
+  def require_owner
+    unless @service_listing.user_id == current_user.id || current_user.admin?
+      redirect_to dashboard_path(tab: "services"), alert: t("flash.service_listings.not_authorized")
+    end
+  end
 
   def service_params
     params.expect(service_listing: [ :title, :description, :contact_info, :category ])
